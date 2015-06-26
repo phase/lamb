759578b4c8e158d44ed783a3e945fe9a7794d987
phase <ph4sesaber@gmail.com>
2015-06-26 16:34:18 -0700

    Better code getter

--- src/lamb.js
+++ src/lamb.js
@@ -9,8 +9,7 @@ function interpret(input){
     //λx.x
     else if(/[a-zA-Z][0-9a-zA-Z]*:(λ[a-zA-Z](.)?)+(?s).*/.test(input)){
         var name = input.split(":")[0];
-        var index = input.split(":")[1].lastIndexOf("λ");
-        var code = input.split(":")[1].substr(index+1);
+        var code = input.match(/((λ.+?)\.)+([^λ].*)/).pop();
         var variables = input.split(":")[1].replace(code, "").replace("λ", "").split(".").join();
         input = "var " + name + " = function(" + variables + "){" + code + "};";
     }
