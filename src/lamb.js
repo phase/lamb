c00bd4b5de97c1dd23c5a45f6a5c961aedd76a27
phase <ph4sesaber@gmail.com>
2015-06-26 15:46:45 -0700

    JS Lamb interpreter

--- src/lamb.js
+++ src/lamb.js
@@ -1,5 +1,22 @@
 //document.querySelectorAll('script[type="text/lamb"]').textContent
 $('input#LambRun').click(function() {
     var text = $('textarea#LambCode').val();
-    $('textarea#LambResult').append(eval(text));
+    $('textarea#LambResult').append(interpret(text) + "<br>");
 });
+
+function interpret(input){
+    if(/[a-zA-Z][0-9a-zA-Z]*(\s)?:(\s)?(")?[0-9a-zA-Z]*(")?/.test(input)){
+        var name = input.split(":")[0].replace(/\s+/, "");
+        var value = input.split(":")[1];
+        input = "var " + name + " = " + value;
+    }
+    //λx.x
+    else if(/[a-zA-Z][0-9a-zA-Z]*:(λ[a-zA-Z](.)?)+(?s).*/.test(input)){
+        var name = input.split(":")[0];
+        var index = input.split(":")[1].lastIndexOf("λ");
+        var code = input.split(":")[1].substr(index+1);
+        var variables = input.split(":")[1].replace(code, "").replace("λ", "").split(".").join();
+        input = "var " + name + " = function(" + variables + "){" + code + "};";
+    }
+    return eval(input);
+}
